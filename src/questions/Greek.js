774e75e8df6e23ba6b3c2b1560402933780c4b0d
Coleen Huang <coleenhuang@gmail.com>
2021-03-30 18:17:35 -0700

    added quiz questions

---
+++ src/questions/Greek.js
@@ -0,0 +1,49 @@
+[
+    {
+        id: 1,
+        question: "Which of these was a name that Paris, prince of Troy, was also known by?",
+        options: [
+            {
+                id: 1,
+                answer: "Alexander"
+            },
+            {
+                id: 2,
+                answer: "Hector"
+            },
+            {
+                id: 3,
+                answer: "Jason"
+            },
+            {
+                id: 4,
+                answer: "Theseus"
+            }
+        ],
+        correct: 1
+    },
+    {
+        id: 2,
+        question: "What was Hestia the goddess of?",
+        options: [
+            {
+                id: 1,
+                answer: "Art and music"
+            },
+            {
+                id: 2,
+                answer: "Wisdom"
+            },
+            {
+                id: 3,
+                answer: "Home and hearth"
+            },
+            {
+                id: 4,
+                answer: "The harvest"
+            }
+
+        ],
+        correct: 3
+    }
+]
---
+++ src/questions/Norse.js
@@ -0,0 +1,94 @@
+[
+    {
+        id: 1,
+        question: "What is Yggdrasil?",
+        options: [
+            {
+                id:1,
+                answer: "A rowan tree"
+            },
+            {
+                id:1,
+                answer: "An ash tree"
+            },
+            {
+                id:1,
+                answer: "A hazel tree"
+            },
+            {
+                id:1,
+                answer: "An oak tree"
+            },
+        ],
+        correct: 2
+    },
+    {
+        id:2,
+        question:"What is the dark mist world called?",
+        options:[
+            {
+                id: 1,
+                answer: "Vanaheim"
+            },
+            {
+                id: 2,
+                answer: "Muspell"
+            },
+            {
+                id: 3,
+                answer: "Alfheim"
+            },
+            {
+                id: 4,
+                answer: "Niflheim"
+            }
+        ],
+        correct: 4,
+    },
+    {
+        id:3,
+        question:"Why does Odin only have one eye?",
+        options:[
+            {
+                id: 1,
+                answer: "It was the price to drink from Mimir's well"
+            },
+            {
+                id: 2,
+                answer: "A raven pecked it out"
+            },
+            {
+                id: 3,
+                answer: "A serpent ate it"
+            },
+            {
+                id: 4,
+                answer: "It was gouged out by Loki"
+            }
+        ],
+        correct: 1,
+    },
+    {
+        id: 4,
+        question:"How did Thor get his hammer back when it was stolen by Thyrm?",
+        options:[
+            {
+                id: 1,
+                answer: "He persuaded Loki to dress as the bride and pretend to be Freya"
+            },
+            {
+                id: 2,
+                answer: "He persuaded Freya to marry Thyrm"
+            },
+            {
+                id: 3,
+                answer: "He asked Loki to steal it back"
+            },
+            {
+                id: 4,
+                answer: "He dressed as the bride, pretending to be Freya"
+            }
+        ],
+        correct: 4,
+    },
+]
