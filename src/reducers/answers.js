a88751c6983164f0a0841107b24b72cc28f80531
Coleen Huang <coleenhuang@gmail.com>
2021-04-02 10:57:44 -0700

    added answersReducer

---
+++ src/reducers/answers.js
@@ -0,0 +1,6 @@
+export default function answersReducer (state=[], action) {
+    switch(action.type){
+        default:
+            return state
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import questionsReducer from './questions';
 import scoreReducer from './score';
+import answersReducer from './answers'
 
 export default combineReducers({
   questionList: questionsReducer,
+  answerList: answersReducer,
   score: scoreReducer
 })
