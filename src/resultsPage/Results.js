fac4d9971600a237fe0378e06f23e74ecae5f9e3
Coleen Huang <coleenhuang@gmail.com>
2021-04-02 20:29:00 -0700

    implemented reset button

--- src/resultsPage/Results.js
+++ src/resultsPage/Results.js
@@ -1,11 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { connect } from 'react-redux';
+import {useHistory} from 'react-router-dom'
 import AnswerKey from './AnswerKey';
 
 
 const Results = ({answerList, questionList}) => {
     const [score, setScore] = useState(0);
-   
+   let history = useHistory();
+
+    const handleClick = () => {
+        setScore(0);
+        history.push('/');
+    }
     return (
         <div>
             Results
@@ -15,10 +21,14 @@ const Results = ({answerList, questionList}) => {
                     let q = questionList.find(question => question.id === answer.questionId);
                     let correctAnswer = q.options.find(a => a.id === q.correct).text
                     let correct = q.correct === answer.answerId
-                    return <AnswerKey questionText={q.questionText} correct={correct} correctAnswer={correctAnswer} addScore={setScore} score={score}/>
+                    return (
+                    <AnswerKey key={`ak${index}`} questionText={q.questionText} 
+                    correct={correct} correctAnswer={correctAnswer} 
+                    addScore={setScore} score={score}/>
+                    )
                    })
             }
-            <button>Reset</button>
+            <button onClick={handleClick}>Reset</button>
         </div>
     )
 }
